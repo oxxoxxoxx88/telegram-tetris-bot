f8c20abf169c3cd508fe69bd2558c467fbafcfac
1 <a1@MacBook-Air-1.local>
2024-09-17 18:05:39 +0300

    Исправлено дублирование объявления переменной canvas

--- game.js
+++ game.js
@@ -1,3 +1,39 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+// Базовые параметры игры
+let gameObjects = [];
+
+// Функция для обновления состояния игры
+function updateGame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Логика падения объектов
+    gameObjects.forEach(obj => {
+        obj.y += obj.speed;
+        if (obj.y > canvas.height) {
+            obj.y = 0;
+        }
+        ctx.fillStyle = obj.color;
+        ctx.fillRect(obj.x, obj.y, obj.size, obj.size);
+    });
+    requestAnimationFrame(updateGame);
+}
+
+// Добавляем объект в игру
+function addObject() {
+    gameObjects.push({
+        x: Math.random() * canvas.width,
+        y: 0,
+        size: 20,
+        speed: 2 + Math.random() * 3,
+        color: 'red'
+    });
+}
+
+// Запускаем игру
+setInterval(addObject, 1000);
+updateGame();
 
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
@@ -126,72 +162,10 @@ function updateGame() {
 
     gameObjects.forEach(obj => {
         obj.y += obj.speed;
-        if (obj.y > canvas.height) {
-            obj.y = 0;
-        }
-        ctx.fillStyle = obj.color;
-        ctx.fillRect(obj.x, obj.y, obj.size, 
-obj.size);
-    });
+      
 
-    console.log('Игра обновляется');
-    requestAnimationFrame(updateGame);
-}
 
-function addObject() {
-    gameObjects.push({
-        x: Math.random() * canvas.width,
-        y: 0,
-        size: 20,
-        speed: 2 + Math.random() * 3,
-        color: 'red'
-    });
-    console.log('Добавлен новый объект');
-}
 
-console.log('Запуск игры');
-setInterval(addObject, 1000);
-updateGame();
-etElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-
-// Базовые параметры игры
-let gameObjects = [];
-
-// Функция для обновления состояния игры
-function updateGame() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Логика падения объектов
-    gameObjects.forEach(obj => {
-        obj.y += obj.speed;
-        if (obj.y > canvas.height) {
-            obj.y = 0;
-        }
-        ctx.fillStyle = obj.color;
-        ctx.fillRect(obj.x, obj.y, obj.size, obj.size);
-    });
-
-    // Вызываем обновление игры снова через requestAnimationFrame
-    requestAnimationFrame(updateGame);
-}
-
-// Добавляем объект в игру
-function addObject() {
-    gameObjects.push({
-        x: Math.random() * canvas.width,
-        y: 0,
-        size: 20,
-        speed: 2 + Math.random() * 3,
-        color: 'red'
-    });
-}
-
-// Устанавливаем безопасный интервал для добавления объектов
-setInterval(addObject, 1000);
-
-// Запускаем игру
-updateGame();
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
@@ -211,8 +185,6 @@ function updateGame() {
         ctx.fillStyle = obj.color;
         ctx.fillRect(obj.x, obj.y, obj.size, obj.size);
     });
-
-    // Вызываем обновление игры снова через requestAnimationFrame
     requestAnimationFrame(updateGame);
 }
 
@@ -227,9 +199,7 @@ function addObject() {
     });
 }
 
-// Устанавливаем безопасный интервал для добавления объектов
-setInterval(addObject, 1000);
-
 // Запускаем игру
+setInterval(addObject, 1000);
 updateGame();
 
